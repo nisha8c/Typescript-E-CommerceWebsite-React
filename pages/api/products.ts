bc2961557c5b0b8fe0045fcabf7755de7597a407
nisha8c <nisha.chavan@appliedtechnology.se>
2023-01-08 19:45:13 +0100

    filtering data

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,9 +1,11 @@
+
 import { initMongoose } from "../../lib/mongoose";
 import Product from '../../models/Product'
 
 const handle = async (request: Request, response: Response) =>{
     await initMongoose();
-    response.json(await Product.find().exec());
+    const data = await Product.find().exec();
+    response.json(data);
 }
 
 export default handle;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   useEffect(() => {
     const getData = async () => {
-      const response = await fetch('http://localhost:3000/api/products');
+      const response = await fetch('/api/products');
       const data = await response.json();
       setProducts(data);
     };
@@ -31,25 +31,41 @@ export default function Home() {
           <button className='btn' type='submit' onClick={() => filterProduct('charger')}>Chargers</button>
       </div>
 
-      <ul className='cards'>
-        {filter.map((product) => {
-          return (
-                <li className='card' key={product.name.toString()}>
-                  <div className='card-header'>
-                    <Image src={product.picture.toString()} alt={product.name.toString()} width={200} height={200}/>
-                  </div>
-                  <h3>{product.name}</h3>
-                  <div>
-                    {product.description}
-                  </div>
-                  <div>
-                    {product.price.toString()}
-                  </div>
+      <div>
+        {filter.map(product => (
+          <div key={product.toString()}>
+           
+              <div>
+                <h2 className="text-2xl py-5 capitalize">{product.category}</h2>
+                <div className="flex -mx-5 overflow-x-scroll snap-x scrollbar-hide">
+
+                    <div key={product.name.toString()} className="px-5 snap-start">
+                    <div className="w-52">
+                      <div className="bg-blue-100 p-5 rounded-xl">
+                        <Image src={product.picture.toString()} alt="" width={200} height={200}/>
+                      </div>
+
+                      <div className="mt-2">
+                        <h3 className="font-bold text-lg">{product.name}</h3>
+                      </div>
+
+                      <p className="text-sm mt-1 leading-4 text-gray-500">{product.description}</p>
+
+                      <div className="flex mt-1">
+                        <div className="text-2xl font-bold grow">${product.price.toString()}</div>
+                        <button className="bg-emerald-400 text-white py-1 px-3 rounded-xl">+</button>
+                      </div>
+
+                    </div>
+                    </div>
                   
-                </li>
-          );
-        })}
-        </ul>
+                </div>
+              </div>
+            
+          </div>
+        ))}
+      </div>
+        
     </>
   );
 }
