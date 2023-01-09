ec2d9b2ca1a688b7f4888a3cc07e49bc36cb8c77
nisha8c <nisha.chavan@appliedtechnology.se>
2023-01-09 12:53:21 +0100

    search works, serversideprops added

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -2,6 +2,10 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { initMongoose } from "../../lib/mongoose";
 import Product from '../../models/Product'
 
+export async function findAllProducts() {
+    return Product.find().exec();
+  }
+  
 const handle = async (_request: NextApiRequest, response: NextApiResponse) =>{
     await initMongoose();
     const data = await Product.find().exec();
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,38 @@
 import React, { useEffect, useState } from 'react';
 import { ProductsData } from '../types/types';
 import Product from '../components/Product';
+import {initMongoose} from "../lib/mongoose";
+import { findAllProducts } from "./api/products";
 
-export default function Home() {
+interface IProductComponentProps {
+  products: ProductsData[]
+}
 
-  const [products, setProducts] = useState<ProductsData[]>([]);
+export default function Home({products}: IProductComponentProps) {
+
   const [filter, setFilter] = useState(products);
+  const [phrase,setPhrase] = useState('');
 
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch('/api/products');
-      const data = await response.json();
-      setProducts(data);
-      setFilter(data);
-    };
-    getData();
-  }, []);
 
   const filterProduct = (cat: string) => {
     const updatedList = () => products.filter((product) => product.category === cat);
     setFilter(updatedList);
   };
 
+  let productInfo;
+  if (phrase) {
+    productInfo = filter.filter(p => p.name.toLowerCase().includes(phrase));
+  } else {
+    productInfo = filter;
+  }
+
   return (
     <section>
+
+      <div className='search-container'>
+        <input value={phrase} onChange={e => setPhrase(e.target.value)} type="text" placeholder="Search for products..." className="bg-gray-200 w-full py-2 px-4 rounded-xl"/>
+      </div>
+
       <div className='buttons'>
           <h2 className="text-2xl py-5 capitalize p-2">Filter Products By Category</h2>
           <button className='btn' type='submit' onClick={() => setFilter(products)}>All</button>
@@ -35,7 +44,7 @@ export default function Home() {
       </div>
 
       <div className='products-container'>
-        {filter.map(product => (
+        {productInfo.map(product => (
           <>      
             <div key={product.name.toString()} className="flex -mx-5">
               <div key={product.toString()} className="px-5 snap-start">
@@ -48,4 +57,14 @@ export default function Home() {
         
     </section>
   );
+}
+
+export async function getServerSideProps() {
+  await initMongoose();
+  const products = await findAllProducts();
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
 }
